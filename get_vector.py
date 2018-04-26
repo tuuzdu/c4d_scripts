b1192a3902f15a4183e64444ac9b5f075cd5982f
tuuzdu <tuuzdu@mail.ru>
2018-04-26 18:57:55 +0300

    get_vector: Takes coordinates of objects and saves it to files

--- get_vector.py
+++ get_vector.py
@@ -2,34 +2,68 @@ import c4d
 from c4d import gui, documents
 
 
-MAX_TIME = 40
-PERIOD = 0.5
+PERIOD = 1
+MAX_INDEX_NAME = 40
+
+def getNames (baseName):
+    names = []
+    for indexName in range(MAX_INDEX_NAME):
+        name = baseName + "{0:d}".format(indexName)
+        names.append(name)
+    return names
+
+def getObjects(objectNames):
+    objects = []
+    for objectName in objectNames:
+        obj = doc.SearchObject(objectName)
+        if obj == None:
+            print ("Object {0:s} doesn't exist".format(objectName))
+            return
+        else:
+            objects.append(obj)
+    return objects
 
 def main():
-    
-    def gototime():
-        doc = documents.GetActiveDocument()
-        #maxTime = doc.GetMaxTime()
-        #print maxTime
-        print ("----------------")
-        obj = doc.SearchObject("Sphere 1")
-        i = 0
-        with open ("coordinates.txt", "w") as f:
-            while i < MAX_TIME:
-                shot_time = c4d.BaseTime(i)
-                #print shot_time.Get()
-                #res = False
-                doc.SetTime(shot_time)
-                #c4d.EventAdd(c4d.EVENT_FORCEREDRAW)
-                c4d.DrawViews(c4d.DRAWFLAGS_ONLY_ACTIVE_VIEW | c4d.DRAWFLAGS_NO_REDUCTION)
-                vec = obj.GetAbsPos()
-                s = "t = {0:.2f} \tx = {1:.2f} \ty = {2:.2f} \tz = {3:.2f}\n".format(i, vec.x, vec.y, vec.z)
-                print s
-                f.write(s)
-                i += PERIOD
-                
-    gototime()
-    
+    baseObjName = "Sphere "
+
+    #doc = documents.GetActiveDocument()
+    maxTime = doc.GetMaxTime().Get()
+    objNames = getNames(baseObjName)
+    objects = getObjects(objNames)
+
+    # try:
+    #     objMaterial = obj.GetFirstTag().GetMaterial()
+    # except AttributeError:
+    #     print ("First object tag must be Material")
+    #     return
+    # objColor = objMaterial.GetAverageColor(c4d.CHANNEL_COLOR)
+    #print objColor
+
+    time = 0
+    pos = []
+    for i in range(MAX_INDEX_NAME):
+        pos.append([])
+
+    while time <= maxTime:
+        shot_time = c4d.BaseTime(time)
+        doc.SetTime(shot_time)
+        c4d.DrawViews(c4d.DRAWFLAGS_ONLY_ACTIVE_VIEW | c4d.DRAWFLAGS_NO_REDUCTION)
+        counter = 0
+        for obj in objects:
+            vec = obj.GetAbsPos()
+            s = "t = {0:.2f} \tx = {1:.2f} \ty = {2:.2f} \tz = {3:.2f}\n".format(time, vec.x, vec.y, vec.z)
+            #print s
+            pos[counter].append(s)
+            counter += 1
+        time += PERIOD
+
+    for i in range(MAX_INDEX_NAME):
+        fileName = objNames[i] + ".txt"
+        with open (fileName, "w") as f:
+            for item in pos[i]:
+                f.write("%s" % item)
+            
+
 
 
 if __name__=='__main__':
