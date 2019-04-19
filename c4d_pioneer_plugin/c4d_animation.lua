3274de4c63e73b750a400a83a68c9ed2d13c3dd2
tuuzdu <tuuzdu@mail.ru>
2019-04-19 10:33:27 +0300

    Colors table, LEDs switch off

--- c4d_pioneer_plugin/c4d_animation.lua
+++ c4d_pioneer_plugin/c4d_animation.lua
@@ -6,6 +6,7 @@ local Animation = {}
 function Animation.new(points_str)
 
 	local strUnpack = string.unpack
+	local tblUnpack = table.unpack
 	local fromHSV = fromHSV
 
 	local state = {stop = 0, idle = 1, flight = 2}
@@ -18,6 +19,15 @@ function Animation.new(points_str)
 		Color.first_led = 4
 		Color.last_led = 28
 		Color.leds = Ledbar.new(29)
+		Color.colors = {	red = 		{1, 0, 0},
+							green = 	{0, 1, 0},
+							blue = 		{0, 0, 1},
+							purple = 	{1, 0, 1},
+							cyan = 		{0, 1, 1},
+							yellow = 	{1, 1, 0},
+							white = 	{1, 1, 1},
+							black = 	{0, 0, 0}
+						}
 
 	function Color.setAllLEDs(r, g, b)
 		for i = 0, Color.last_led, 1 do
@@ -31,8 +41,8 @@ function Animation.new(points_str)
 		end
 	end
 
-	local Coord = {}
-		Coord.setCoord = ap.goToLocalPoint
+	local Position = {}
+		Position.setPosition = ap.goToLocalPoint
 
 	local Point = {}
 		Point.points_str_size = string.packsize(str_format)
@@ -61,6 +71,7 @@ function Animation.new(points_str)
 		Config.last_index = cfg.last_index or points_count
 		Config.t_after_prepare = cfg.time_after_prepare or 7
 		Config.t_after_takeoff = cfg.time_after_takeoff or 8
+		Config.t_leds_after_fail = cfg.time_leds_after_fail or 30
 		Config.lat = cfg.lat or origin_lat
 		Config.lon = cfg.lon or origin_lon
 		if Config.lat ~= nil and Config.lon ~= nil then
@@ -73,8 +84,13 @@ function Animation.new(points_str)
 		if self.state ~= state.stop then
 			if e == Ev.SYNC_START then
 				self.global_time_0 = getGlobalTime() + Config.t_after_prepare + Config.t_after_takeoff
-				Color.setInfoLEDs(0, 0, 1)
+				Color.setInfoLEDs(tblUnpack(Color.colors.blue))
 				self:animInit()
+			end
+			if (e == Ev.CONTROL_FAIL or e == Ev.ENGINE_FAIL or e == Ev.SHOCK or e == Ev.COPTER_DISARMED) then
+				self.state = state.stop
+				Color.setAllLEDs(tblUnpack(Color.colors.black))
+				Timer.callLater(Config.t_leds_after_fail, function () Color.setInfoLEDs(tblUnpack(Color.colors.yellow)) end)
 			end
 		end
 	end
@@ -83,7 +99,7 @@ function Animation.new(points_str)
 		self.state = state.flight
 		ap.push(Ev.MCE_PREFLIGHT) 
 		sleep(Config.t_after_prepare)
-		Color.setInfoLEDs(0, 0, 0)
+		Color.setInfoLEDs(tblUnpack(Color.colors.black))
 		ap.push(Ev.MCE_TAKEOFF) -- Takeoff altitude should be set by AP parameter
 		self.point_current = Point.getPoint(Config.init_index)
 		self.t_init = self.point_current[1]
@@ -97,9 +113,9 @@ function Animation.new(points_str)
 			self.point_current = Point.getPoint(point_index + 1)
 			local t = self.point_current[1]
 			Color.setAllLEDs(r, g, b)
-			Coord.setCoord(x, y, z)
+			Position.setPosition(x, y, z)
 			Timer.callAtGlobal(self.global_time_0 + t - self.t_init, function () self:animLoop(point_index + 1) end)
-		else
+		elseif self.state == state.flight then
 			local t = self.point_current[1]
 			local delay = 1
 			Timer.callAtGlobal(self.global_time_0 + t + delay - self.t_init, function () self:landing() end)
@@ -108,8 +124,8 @@ function Animation.new(points_str)
 	
 	function obj:landing()
 		if Config.light_onlanding == false then
-			Color.setAllLEDs(0, 0, 0)
-			Color.setInfoLEDs(1, 0, 1)
+			Color.setAllLEDs(tblUnpack(Color.colors.black))
+			Color.setInfoLEDs(tblUnpack(Color.colors.purple))
 		end
 		self.state = state.landing
 		ap.push(Ev.MCE_LANDING)
@@ -123,7 +139,7 @@ function Animation.new(points_str)
 			local leap_second = 19
 			local t_period = 15 -- time window
 			local t_near = leap_second + t_period*(math.floor((t - leap_second)/t_period) + 1)
-			Color.setInfoLEDs(0, 1, 0)
+			Color.setInfoLEDs(tblUnpack(Color.colors.green))
 			Timer.callAtGlobal(t_near, function () self:eventHandler(Ev.SYNC_START) end)
 		else
 			Timer.callLater(0.2, function () self:waitStartLoop() end)
@@ -131,7 +147,7 @@ function Animation.new(points_str)
 	end
 
 	function obj:spin()
-		Color.setInfoLEDs(1, 0, 0)
+		Color.setInfoLEDs(tblUnpack(Color.colors.red))
 		self:waitStartLoop()
 		-- self:start() -- For debugging
 	end
